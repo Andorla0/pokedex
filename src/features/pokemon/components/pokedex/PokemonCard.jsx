f908a333365ef31ad8a778ed9adfbf294789de30
Andorla0 <84116120+Andorla0@users.noreply.github.com>
2026-03-20 12:33:10 -0500

    :building_construction: refactor: sub-categorize pokemon components by context

---
+++ src/features/pokemon/components/pokedex/PokemonCard.jsx
@@ -0,0 +1,44 @@
+import { Link } from "react-router"
+import PokemonTypeBadge from "../ui/PokemonTypeBadge"
+
+function PokemonCard({id,name,image,types}) {
+    
+    return (
+        <Link to={`/pokemon/${id}`} className="w-full">
+            <div className="group  bg-white rounded-2xl border border-gray-200 px-3 py-5 shadow-2xl hover:shadow-sm  transition-all duration-200 w-full">
+            
+                <div className="flex justify-center items-center mb-4">
+                    <img
+                    src={image}
+                    alt={name}
+                    className="w-24 h-24 object-contain group-hover:scale-125 transition-transform duration-500"
+                    />
+                </div>
+
+                <p className="text-sm text-gray-400 text-center mb-1">
+                    #{id}
+                </p>
+
+                <h3 className="text-lg font-semibold text-gray-900 text-center capitalize mb-2">
+                    {name}
+                </h3>
+
+                <div className="flex justify-center gap-2">
+                    {types.map((type) => (
+                    <span
+                        key={type}
+                        
+                    >
+                        <PokemonTypeBadge type={type} />
+                    </span>
+                    ))}
+                </div>
+
+            </div>
+        </Link>
+       
+    )
+
+}
+
+export default PokemonCard
--- src/features/pokemon/components/pokedex/PokemonFilters.jsx
+++ src/features/pokemon/components/pokedex/PokemonFilters.jsx
@@ -1,5 +1,6 @@
 
-import { usePokemonTypes } from '../hooks/usePokemons';
+import { usePokemonTypes } from '../../hooks/usePokemons';
+import { TYPE_COLORS } from '../../utils/pokemonTypes';
 
 function PokemonFilters({selectedType, onTypeChange }) {
     
@@ -9,7 +10,7 @@ function PokemonFilters({selectedType, onTypeChange }) {
 
 
     if (isLoading) return <p>Loading types...</p>;
-    console.log(POKEMON_TYPES)
+
     return (
         <div className="mb-6 max-w-4xl mx-auto">
             <div className="flex items-center gap-2 mb-3">
@@ -18,19 +19,24 @@ function PokemonFilters({selectedType, onTypeChange }) {
             </div>
 
             <div className="flex gap-1.5 flex-wrap pb-2">
-                {POKEMON_TYPES.map((type) => (
-                <button
+                 {POKEMON_TYPES.map((type) => {
+                const isSelected = selectedType === type;
+                const bg = TYPE_COLORS[type] ?? "bg-gray-300";
+
+                return (
+                    <button
                     key={type}
                     onClick={() => onTypeChange(type)}
-                    className={`px-3 py-1 text-sm whitespace-nowrap rounded-full border-2 transition-all capitalize ${
-                    selectedType === type
-                        ? 'bg-gray-900 text-white border-gray-900'
+                    className={`px-3 py-1 text-sm whitespace-nowrap rounded-full border-2 transition-all capitalize font-medium
+                        ${isSelected
+                        ? `${bg} text-white border-transparent`
                         : 'bg-white text-gray-700 border-gray-300 hover:border-gray-400'
-                    }`}
-                >
+                        }`}
+                    >
                     {type}
-                </button>
-                ))}
+                    </button>
+                );
+                })}
             </div>
         </div>
 
