e9784d64b2239a1705f1c737f166559f9009d7cd
Andorla0 <84116120+Andorla0@users.noreply.github.com>
2026-03-20 14:53:36 -0500

    :lipstick: style: update tailwind classes with custom ones

--- src/features/pokemon/components/pokedex/PokedexHeader.jsx
+++ src/features/pokemon/components/pokedex/PokedexHeader.jsx
@@ -6,20 +6,19 @@ function PokedexHeader({pokemonCount, searchPokemon, onSearchChange}) {
 
     return (
 
-    <header className="w-full bg-[#de5239]">
-        {/* <div className="max-w-5xl mx-auto sm:px-6 md:px-10 lg:px-12 xl:px-16 p-2"> */}
+    <header className="w-full bg-pokedex-header">
         <div className="max-w-5xl mx-auto sm:px-6 md:px-10 lg:px-12 xl:px-16 p-2">
             <div className="w-full flex flex-col gap-2">
                 <div className="flex flex-col gap-4 sm:flex-row sm:items-start sm:justify-between">
                     <div>
                         <h1 className="text-xl font-bold sm:text-2xl md:text-3xl text-white">Pokedex</h1>
-                        <p className=" text-xs md:text-sm text-[#ffd57b]">
+                        <p className=" text-xs md:text-sm text-pokedex-yellow">
                             A simple pokedex app built with React and PrimeReact :)
                         </p>
                     
                     </div>
 
-                    <div className="sm:text-right text-[#ffd57b]">
+                    <div className="sm:text-right text-pokedex-yellow">
                     <p className="text-xs md:text-sm">Total Pokemon</p>
                     <p className="text-xl font-bold sm:text-2xl md:text-3xl text-white">{pokemonCount}</p>
                 </div>
@@ -32,9 +31,9 @@ function PokedexHeader({pokemonCount, searchPokemon, onSearchChange}) {
                     value={searchPokemon}
                     onChange={(e)=> onSearchChange(e.target.value)}
                     placeholder="Search Pokemon by name"
-                    className="w-full bg-[#fff8f0] text-[#3d1a00] pl-4"
+                    className="w-full bg-pokedex-bg text-pokedex-text pl-4"
                     />
-                    <Button icon="pi pi-search" className="bg-[#ffad52] border-[#ffad52] " />
+                    <Button icon="pi pi-search" className="bg-pokedex-accent border-pokedex-accent " />
                 </div>
                 </div>
             </div>
