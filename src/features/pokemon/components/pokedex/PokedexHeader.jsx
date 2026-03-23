26dbd036cbc006dfdb61279c95aa684573e0a412
Andorla0 <84116120+Andorla0@users.noreply.github.com>
2026-03-23 04:01:12 -0500

    :lipstick: style: add an icon for header

--- src/features/pokemon/components/pokedex/PokedexHeader.jsx
+++ src/features/pokemon/components/pokedex/PokedexHeader.jsx
@@ -11,18 +11,17 @@ function PokedexHeader({pokemonCount, searchPokemon, onSearchChange}) {
             <div className="w-full flex flex-col gap-2">
                 <div className="flex flex-col gap-4 sm:flex-row sm:items-start sm:justify-between">
                     <div>
-                        <h1 className="text-xl font-bold sm:text-2xl md:text-3xl text-white">Pokedex</h1>
+                        <h1 className="text-xl font-bold sm:text-2xl md:text-3xl text-white"> <i className="pi pi-mobile text-white text-xl md:text-2xl mr-1" />Pokedex</h1>
                         <p className=" text-xs md:text-sm text-pokedex-yellow">
                             A simple pokedex app built with React and PrimeReact :)
                         </p>
-                    
                     </div>
 
                     <div className="sm:text-right text-pokedex-yellow">
-                    <p className="text-xs md:text-sm">Total Pokemon</p>
-                    <p className="text-xl font-bold sm:text-2xl md:text-3xl text-white">{pokemonCount}</p>
+                        <p className="text-xs md:text-sm">Total Pokemon</p>
+                        <p className="text-xl font-bold sm:text-2xl md:text-3xl text-white">{pokemonCount}</p>
+                    </div>
                 </div>
-            </div>
 
             <div className="w-full h-10">
                 <div className="flex gap-3 w-full h-full px-0 sm:px-2">
