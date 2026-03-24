37f1bf34c63e1a099d3843eda0ad3c43c548516c
Andorla0 <84116120+Andorla0@users.noreply.github.com>
2026-03-24 01:22:54 -0500

    :sparkles: feat: add new app routes for battle views

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -2,12 +2,19 @@ import { Routes, Route } from "react-router"
 import PokedexPage from "../pages/pokedex/PokedexPage"
 import PokemonDetailsPage from "../pages/pokemon/PokemonDetailsPage"
 import '@fortawesome/fontawesome-free/css/all.min.css';
+import BattlesPage from "../pages/battles/BattlesPage";
+import CreateBattlePage from "../pages/battles/CreateBattlePage";
+import BattleDetailPage from "../pages/battles/BattleDetailPage";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<PokedexPage />} />
       <Route path="/pokemon/:id" element={<PokemonDetailsPage/>} />
+      <Route path="/battles" element={<BattlesPage />} />
+      <Route path="/battles/new" element={<CreateBattlePage />} />
+      <Route path="/battles/:id" element={<BattleDetailPage />} />
+      <Route path="/battles/:id/edit" element={<CreateBattlePage />} />
     </Routes>
   )
 }
