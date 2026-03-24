a2eef4b579a7349a17cc7a6675b79cc2a7259653
Andorla0 <84116120+Andorla0@users.noreply.github.com>
2026-03-24 01:27:36 -0500

    ✨ feat: add battles navigation button

--- src/features/pokemon/components/pokedex/PokedexHeader.jsx
+++ src/features/pokemon/components/pokedex/PokedexHeader.jsx
@@ -1,29 +1,33 @@
 
 import { InputText } from "primereact/inputtext";
 import { Button } from 'primereact/button';
-import { Dropdown } from "primereact/dropdown";
-function PokedexHeader({pokemonCount, searchPokemon, onSearchChange}) {
+import { useNavigate } from "react-router";
 
+function PokedexHeader({searchPokemon, onSearchChange}) {
+    const navigate = useNavigate();
     return (
 
     <header className="w-full bg-pokedex-header">
         <div className="max-w-5xl mx-auto sm:px-6 md:px-10 lg:px-12 xl:px-16 p-2">
             <div className="w-full flex flex-col gap-2">
-                <div className="flex flex-col gap-4 sm:flex-row sm:items-start sm:justify-between">
+                <div className="flex flex-col gap-4 sm:flex-row sm:items-center sm:justify-between">
                     <div>
-                        <h1 className="text-xl font-bold sm:text-2xl md:text-3xl text-white"> <i className="pi pi-mobile text-white text-xl md:text-2xl mr-1" />Pokedex</h1>
-                        <p className=" text-xs md:text-sm text-pokedex-yellow">
+                        <h1 className="text-xl font-bold sm:text-2xl md:text-3xl text-white"> 
+                            <i className="pi pi-mobile text-white text-xl md:text-2xl mr-1" />
+                            Pokedex
+                        </h1>
+                        <p className="text-xs md:text-sm text-pokedex-yellow">
                             A simple pokedex app built with React and PrimeReact :)
                         </p>
                     </div>
 
-                    <div className="sm:text-right text-pokedex-yellow">
-                        <p className="text-xs md:text-sm">Total Pokemon</p>
-                        <span className="inline-flex items-center gap-1  bg-pokedex-yellow/10 px-3  rounded-lg ">
-                        <i className="pi pi-globe text-pokedex-accent text-xs md:text-xl" />
-                        <p className="text-lg font-bold sm:text-xl md:text-2xl text-white">{pokemonCount}</p>
-
-                        </span>
+                    <div className="flex">
+                        <Button
+                            label="Battles"
+                            icon="fa-solid fa-bolt"
+                            className="bg-pokedex-yellow border-pokedex-yellow text-pokedex-text font-bold"
+                            onClick={() => navigate('/battles')}
+                        />
                     </div>
                 </div>
 
@@ -40,6 +44,7 @@ function PokedexHeader({pokemonCount, searchPokemon, onSearchChange}) {
                 </div>
                 </div>
             </div>
+            
             </div>
         </div>
     </header>
